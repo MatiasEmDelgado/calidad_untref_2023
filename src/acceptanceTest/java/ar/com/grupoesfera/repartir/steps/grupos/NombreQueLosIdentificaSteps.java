751da056a6a7fbe718f95125a001fb0c8d410047
Matías Delgado <matias.untref@gmail.com>
2023-04-08 21:06:50 -0300

    El Escenario grupo sin nombre me habia dado verde por algun cache anterior, se arregla el caso para que de todo verde

--- src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/NombreQueLosIdentificaSteps.java
+++ src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/NombreQueLosIdentificaSteps.java
@@ -53,16 +53,30 @@ public class NombreQueLosIdentificaSteps extends CucumberSteps {
     @Cuando("el usuario intenta crear un grupo sin indicar su nombre")
     public void elUsuarioIntentaCrearUnGrupoSinIndicarSuNombre() {
 
-        var crearGruposButton = driver.findElement(By.id("crearGruposButton"));
+       // var crearGruposButton = driver.findElement(By.id("crearGruposButton"));
+      //  crearGruposButton.click();
+
+      //  var miembrosInput = driver.findElement(By.id("miembrosGrupoNuevoInput"));
+      //  miembrosInput.sendKeys("Carla");
+       // miembrosInput.sendKeys(Keys.ENTER);
+      //  miembrosInput.sendKeys("Miguel");
+      //  miembrosInput.sendKeys(Keys.ENTER);
+
+       // driver.findElement(By.id("guardarGrupoNuevoButton")).click();
+
+        var wait = new WebDriverWait(driver, 2);
+        var crearGruposButton = wait.until(elementToBeClickable(By.id("crearGruposButton")));
         crearGruposButton.click();
 
         var miembrosInput = driver.findElement(By.id("miembrosGrupoNuevoInput"));
-        miembrosInput.sendKeys("Carla");
+        miembrosInput.sendKeys("Victor");
         miembrosInput.sendKeys(Keys.ENTER);
-        miembrosInput.sendKeys("Miguel");
+        miembrosInput.sendKeys("Brenda");
         miembrosInput.sendKeys(Keys.ENTER);
 
         driver.findElement(By.id("guardarGrupoNuevoButton")).click();
+
+        wait.until(visibilityOfElementLocated(By.id("mensajesToast")));
     }
 
     @Cuando("el usuario intenta crear un grupo indicando el nombre con unico caracter {string}")
@@ -97,12 +111,9 @@ public class NombreQueLosIdentificaSteps extends CucumberSteps {
     }
     @Entonces("no debería crear el grupo sin nombre")
     public void noDeberiaCrearElGrupoSinNombre() {
-
         var grupoTR = driver.findElements(By.cssSelector("app-grupos table tr"));
-        assertThat(grupoTR).hasSize(0);
-
-        var campoTDs = grupoTR.get(1).findElements(By.tagName("td"));
-        assertThat(campoTDs.get(0)).isNull();
+        var campoTDs = grupoTR.get(0).findElements(By.tagName("td"));
+        assertThat(campoTDs).hasSize(0);
     }
 
     @Y("debería informarce que no se puede crear un grupo con nombre de un único caracter")
@@ -112,14 +123,14 @@ public class NombreQueLosIdentificaSteps extends CucumberSteps {
     @Y("debería ser informado que no puede crear un grupo sin nombre")
     public void deberiaSerInformadoQueNoPuedeCrearUnGrupoSinNombre() {
 
-        //var wait = new WebDriverWait(driver, 2);
-        //var mensajesToast = wait.withMessage("Mostro Toast")
-               // .until(visibilityOfElementLocated(By.id("mensajesToast")));
-        //wait.withMessage("Título del Toast es 'Error'")
-                //.until(textToBePresentInElement(mensajesToast, "Error"));
-        //assertThat(mensajesToast.getText())
-                //.as("Descripción del Toast")
-                //.contains("No se puede guardar");
+        var wait = new WebDriverWait(driver, 2);
+        var mensajesToast = wait.withMessage("Mostro Toast")
+                .until(visibilityOfElementLocated(By.id("mensajesToast")));
+        wait.withMessage("Título del Toast es 'Error'")
+                .until(textToBePresentInElement(mensajesToast, "Error"));
+        assertThat(mensajesToast.getText())
+                .as("Descripción del Toast")
+                .contains("No se puede guardar");
     }
 
 }
